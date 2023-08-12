f3f56a7ec946df5c1887732de40e6a8d4d20065f
Sherelle <sherellie1@hotmail.com>
2023-08-12 23:32:31 +0100

    array of codes added for getCurrencyCode() method

---
+++ src/main/java/com/codingblackfemales/BasicCurrencyConverter.java
@@ -0,0 +1,34 @@
+package com.codingblackfemales;
+public class BasicCurrencyConverter {
+    // impliment the currency converter interface
+
+    use CurrenciesGBP
+    to access  currency rates.
+
+
+    try(){
+
+    }catch{
+/*invalid or missing  source code
+ * invalid or missing destination code
+ * invalid or null amount
+*/
+    }
+
+    /*
+     * PERAMETERS
+convert currency ()
+get exchange rates()
+get currency codes()
+
+
+RESULTS
+
+UI when user selects thes perameters getters get the the data that has been selected.
+
+EXAMPLES
+
+
+PSUDOCODE
+     */
+}
---
+++ src/main/java/com/codingblackfemales/CurrencyConverter.java
@@ -0,0 +1,101 @@
+package com.codingblackfemales;
+
+/*PREP
+ * 
+ * PERAMETERS
+
+
+RESULTS
+
+
+EXAMPLES
+
+
+PSUDOCODE
+ */
+
+public class CurrencyConverter {
+
+
+    public double convertCurrency(String sourceCurrencyCode, String destinationCurrencyCode, double amount){
+        // if(sourceCurrencyCode != destinationCurrencyCode){
+        //     return amount * getExchangeRate();
+        // }else{
+
+        // }
+        
+        /*
+     * PERAMETERS source currency code, destination currency code , amount to convert to.
+     *
+
+
+RESULTS
+
+source currency code is selected,
+destination currency code is selected,
+user inputs the amount.
+
+
+EXAMPLES
+
+
+PSUDOCODE
+    //  */
+    //     double convertedAmount = 0;
+    //     try{
+
+    //     }catch{
+    //         System.err.println(err);
+    //     }
+    //     return convertedAmount;
+    }
+
+
+    public String[] getCurrencyCodes(){
+
+        String[] currencyCode = {"GBP","USD","JYP","EUR"};
+        return currencyCode;
+        
+        }
+         /*
+     * PERAMETERS
+get currency codes,
+an array of currency codes is given to traverse for the user to selec from.
+
+RESULTS
+getter that gets the currency code once user has selected it.
+
+
+EXAMPLES
+user selects GBP as a currency and it gets hat currency
+user selcets USD as another currency, the code returns that currency
+
+
+PSUDOCODE
+     */
+    
+    public double getExchangeRate(String sourceCurrencyCode, String destinationCurrencyCode){
+
+        if(sourceCurrencyCode != destinationCurrencyCode){
+            return 
+        }
+ /*
+     * PERAMETERS
+source currency code
+destination currency code.
+
+once the two have been selected the code return the exchange rate for the currency conversion
+
+RESULTS
+
+
+EXAMPLES
+
+
+PSUDOCODE
+     */
+    }
+
+
+    
+}
