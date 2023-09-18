273fe29c5eb7477c333d6206fc8aac410291b69f
Sherelle <sherellie1@hotmail.com>
2023-09-18 14:21:24 +0100

    restarting assignment, switching to new branch

--- src/main/java/com/codingblackfemales/Main.java
+++ src/main/java/com/codingblackfemales/Main.java
@@ -1,7 +1,12 @@
 package com.codingblackfemales;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        CurrenciesGBP currenciesGBP = new CurrenciesGBP();
+        System.out.println(currenciesGBP.getAllExchangeRates());
+        
     }
 }
