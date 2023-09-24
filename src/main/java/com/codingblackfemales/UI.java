69b68b1545f7e09dd6a8d229cefdd2c82ac04db9
Sherelle <sherellie1@hotmail.com>
2023-09-24 22:52:09 +0100

    scanner issues, i give up

--- src/main/java/com/codingblackfemales/UI.java
+++ src/main/java/com/codingblackfemales/UI.java
@@ -1,16 +1,21 @@
 package com.codingblackfemales;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import com.codingblackfemales.exceptions.ExceededRequestLimit;
-import com.codingblackfemales.exceptions.InsufficientAmountEntered;
+import com.codingblackfemales.exceptions.ExchangeRateUnavailable;
 
 public class UI {
+
+    String destinationCurrencyCode =  "";
+    String sourceCurrencyCode = "";
 
     public void checkAmount(int numberOfConversions){
         try (Scanner scanner = new Scanner(System.in)) {
             if(numberOfConversions > 1 || numberOfConversions < 1){
-            
                 if(numberOfConversions > 10 || numberOfConversions < 1){
                 System.out.println("I'm sorry, you have exceeded the number of legal request per search. please type a number less than 10");
                 numberOfConversions = scanner.nextInt();
@@ -20,18 +25,22 @@ public class UI {
                 }
             }
         }
-        }
+    }
 
      public void startLiveConverter(int numberOfConversions){
         Using using = new Using();
-        try (Scanner scanner = new Scanner(System.in)) {
-            for(int i = 0; i <= numberOfConversions; i++){
+        ArrayList<String> conversions = new ArrayList<>();
+        
+        Scanner scanner = new Scanner(System.in);
 
+            for(int i = 0; i <= numberOfConversions; i++){
+            
+            scanner.nextLine();
             System.out.println("Please enter your chosen destination currency.");
-            String destinationCurrencyCode = scanner.nextLine().toUpperCase();
+            destinationCurrencyCode = scanner.nextLine().toUpperCase();
 
             System.out.println("Please enter the code of the currency you wish to convert from.");
-            String sourceCurrencyCode = scanner.nextLine().toUpperCase();
+            sourceCurrencyCode = scanner.nextLine().toUpperCase();
 
             double transactionExchangeRate = 0.0;
             using.checkAPI(sourceCurrencyCode, sourceCurrencyCode);
@@ -39,30 +48,24 @@ public class UI {
             System.out.println("Please enter the amount that you would like to convert.");       
             double amount = scanner.nextDouble();
 
+            using.checkAmount(amount);
+
             UseRatesAPI useAPI = new UseRatesAPI();
-            using.timeDelay(250);
-            String formatRate = String.format("%.2f", transactionExchangeRate);
-            System.out.println("The live exchange rate for this transaction is " + formatRate+"%.");
+            // String formatRate = String.format("%.2f", transactionExchangeRate);
+            // System.out.println("The live exchange rate for this transaction is " + formatRate+"%.");
             double apiConvertedCurrency = useAPI.apiConversion(transactionExchangeRate, amount);
             String formatTotal = String.format("%.2f", apiConvertedCurrency);
-            using.timeDelay(500);
             String result = amount +" " +sourceCurrencyCode + " converts to "+formatTotal +" "+ destinationCurrencyCode;
+            conversions.add(result);
             }
-        } catch (InsufficientAmountEntered e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
 
+            for(String completeConversions : conversions){
+                System.out.println(conversions);
+                using.goodbye();
+            }
         
-
-         // depending on which converter the user has chosen we use a different class and method to convert their currency.
-             
-
-            using.timeDelay(600);
-            System.out.println("We hope you enjoyed your experience with Currency Converter today!");
-    
-}
-
-    public void checkAmount() {
+            System.out.println("I'm sorry, we are having issues with our live rate system at the moment. Please try again later.");
+            scanner.close();
+        
     }
 }
--- src/main/java/com/codingblackfemales/UserInterface.java
+++ src/main/java/com/codingblackfemales/UserInterface.java
@@ -28,16 +28,15 @@ public class UserInterface {
 
         using.selectConverter(scanner);        
 
-        boolean originalRoute = true;
-
         using.timeDelay(500);
         System.out.println("Please type the number of conversions you would like to make. You can convert a maximum number of 10 currencies at once. Note that for more than one conversion you will need to use the live rates converter\n");
         int numberOfConversions = scanner.nextInt();
-        ui.checkAmount();
+        ui.checkAmount(numberOfConversions);
         scanner.nextLine();
 
 
         if(numberOfConversions != 1){
+            scanner.nextLine();
             ui.startLiveConverter(numberOfConversions);
         }
         
@@ -54,55 +53,47 @@ public class UserInterface {
 
  
 
-                // logic for using the original currency converter.
-                boolean destinationAvailable = basicConverter.checkCode(destinationCurrencyCode); 
-                boolean sourceAvailable = basicConverter.checkCode(sourceCurrencyCode);
+        // logic for using the original currency converter.
+        boolean destinationAvailable = basicConverter.checkCode(destinationCurrencyCode); 
+        boolean sourceAvailable = basicConverter.checkCode(sourceCurrencyCode);
 
-                using.ratesUnavailable(sourceAvailable, destinationAvailable, scanner);
+        using.ratesUnavailable(sourceAvailable, destinationAvailable, scanner);
 
-                    if(!sourceAvailable || !destinationAvailable){
-                    using.timeDelay(250);
-                    System.out.println("Would you like to search the live rate database?");
-                    using.timeDelay(500);
-                     System.out.println("Please type your answer yes or no");
-                    using.checkRatesAPI(scanner);
+        if(!sourceAvailable || !destinationAvailable){
+        using.timeDelay(250);
+        System.out.println("Would you like to search the live rate database?");
+        using.timeDelay(500);
+        System.out.println("Please type your answer yes or no");
                 
-                        if(using.checkRatesAPI(scanner).equalsIgnoreCase("yes")){
-                        System.out.println("Please note these rates are live and may fluctuate hourly. Currency converter cannot guarantee a rate you may have seen earlier if you decide not to proceed with yor transaction.");
-                        ui.startLiveConverter(numberOfConversions);
-                        }else{
-                            System.out.println("Thank you for using Currency Converter!");
-                            System.exit(0);
-                        }
-                    }else{
-                    System.out.println("Please enter the amount that you would like to convert.");       
-                    double amount = scanner.nextDouble();
-
-                    using.confirmTransaction(scanner, sourceCurrencyCode, sourceCurrencyCode, amount);
+            if(!using.checkRatesAPI(scanner).equalsIgnoreCase("yes")){
+                        
+                System.out.println("Thank you for using Currency Converter!");
+                System.exit(0);
+            }else{
+                System.out.println("Please note these rates are live and may fluctuate hourly. Currency converter cannot guarantee a rate you may have seen earlier if you decide not to proceed with yor transaction.");
+                ui.startLiveConverter(numberOfConversions);
+            }
+        }else{
+        System.out.println("Please enter the amount that you would like to convert.");       
+        double amount = scanner.nextDouble();
                    
 
-                        if(using.confirmTransaction(scanner, sourceCurrencyCode, sourceCurrencyCode, amount).equalsIgnoreCase("yes")){
-                         scanner.close();
-                            double transactionExchangeRate = basicConverter.getExchangeRate(sourceCurrencyCode, destinationCurrencyCode);
-                            using.timeDelay(250);
-                            String formatRate = String.format("%.2f", transactionExchangeRate);
-                            System.out.println("The exchange rate for this transaction is " + formatRate);
-                            double convertedCurrency = basicConverter.convertCurrency(sourceCurrencyCode, destinationCurrencyCode, amount);
-                            String formatTotal = String.format("%.2f", convertedCurrency);
-                            using.timeDelay(500);
-                            System.out.println("This amount converts to "+formatTotal +" "+ destinationCurrencyCode);
-                        } else {
+            if(using.confirmTransaction(scanner, sourceCurrencyCode, sourceCurrencyCode, amount).equalsIgnoreCase("yes")){
+                scanner.close();
+                double transactionExchangeRate = basicConverter.getExchangeRate(sourceCurrencyCode, destinationCurrencyCode);
+                using.timeDelay(250);
+                String formatRate = String.format("%.2f", transactionExchangeRate);
+                System.out.println("The exchange rate for this transaction is " + formatRate);
+                double convertedCurrency = basicConverter.convertCurrency(sourceCurrencyCode, destinationCurrencyCode, amount);
+                String formatTotal = String.format("%.2f", convertedCurrency);
+                using.timeDelay(500);
+                System.out.println("This amount converts to "+formatTotal +" "+ destinationCurrencyCode);
+            } else {
                 using.timeDelay(100);
                 System.out.println("Apologies for the inconvenience, please start again.");
                 System.exit(0);
             }
-
-
-            }
-            
-            using.goodbye();
         }
+    using.goodbye();
     }
-
-    
- 
+}
